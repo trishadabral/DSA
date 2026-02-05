422800953dff620db89be9cb6775756aaa990652
Trisha Dabral <trishadabral@gmail.com>
2026-02-05 22:39:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (31.11%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        int n=*max_element(heights.begin(),heights.end());
+        vector<int> expected=heights;
+        sort(expected.begin(),expected.end());
         int c=0;
-        vector<int>cf(n+1);
-        vector<int>sorted(heights.size());
-        for(int x:heights)cf[x]++;
-        for(int i=1;i<n+1;i++)cf[i]+=cf[i-1];
-        for(int i=heights.size()-1;i>=0;i--){
-            if(cf[heights[i]]!=0){
-                cf[heights[i]]--;
-                sorted[cf[heights[i]]]=heights[i];
-            }
-        }
-        for(int i=0;i<heights.size();i++){
-            if(heights[i]!=sorted[i])c++;
+        for (int i=0;i<heights.size();i++) {
+            if (heights[i]!=expected[i]) c++;
         }
         return c;
     }
