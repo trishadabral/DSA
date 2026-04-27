2c23d01bf4e231220d0cb55b7d611a3f597c66f5
Trisha Dabral <trishadabral@gmail.com>
2026-04-27 14:00:11 +0530

    Time: 3 ms (95.70%) | Memory: 19.6 MB (58.98%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -3,6 +3,7 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int n=nums.size();
         if(n<3)return n;
+        // if element is less than 3 so no need to rearrange
         int j=2;
         for(int i=2;i<n;i++){
             if(nums[i]!=nums[j-2]){
