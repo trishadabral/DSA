efe651b53631575d015970e572e001e60378a861
Trisha Dabral <trishadabral@gmail.com>
2026-08-03 13:48:40 +0530

    Time: 26 ms (65.28%) | Memory: 136.1 MB (83.46%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -2,26 +2,22 @@ class Solution {
 public:
     int n;
     vector<int>dp;
-    int solve(vector<int>& stoneValue,int i){
-        if(i>=n)return 0;
-        if(dp[i]!=INT_MIN)return dp[i];
-        int res=INT_MIN;
-        res=max(res,stoneValue[i]-solve(stoneValue,i+1));
-        if(i+1<n){
-            res=max(res,stoneValue[i]+stoneValue[i+1]-solve(stoneValue,i+2));
-        }
-        if(i+2<n){
-            res=max(res,stoneValue[i]+stoneValue[i+1]+stoneValue[i+2]-solve(stoneValue,i+3));
-        }
-        return dp[i]=res;
-
-    }
     string stoneGameIII(vector<int>& stoneValue) {
         n=stoneValue.size();
-        dp.resize(n+1,INT_MIN);
-        int k=solve(stoneValue,0);
-        if(k>0)return "Alice";
-        else if(k<0)return "Bob";
+        dp.resize(n+3,0);
+        for(int i=n-1;i>=0;i--){
+            dp[i] = INT_MIN;
+            dp[i]=max(dp[i],stoneValue[i]-dp[i+1]);
+            if(i+1<n){
+                dp[i]=max(dp[i],stoneValue[i]+stoneValue[i+1]-dp[i+2]);
+            }
+            if(i+2<n){
+                dp[i]=max(dp[i],stoneValue[i]+stoneValue[i+1]+stoneValue[i+2]-dp[i+3]);
+            }
+            
+        }
+        if(dp[0]>0)return "Alice";
+        else if(dp[0]<0)return "Bob";
         else return "Tie";
     }
 };
