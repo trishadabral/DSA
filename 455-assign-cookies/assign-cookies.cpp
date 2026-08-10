c506e07ec6d307e6993470aab728a8c7a863bbae
Trisha Dabral <trishadabral@gmail.com>
2026-08-10 09:49:30 +0530

    Time: 5 ms (79.98%) | Memory: 44.8 MB (48.81%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n=g.size(),m=s.size(),c=0,i=0,j=0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        while(i<n && j<m){
+            if(g[i]<=s[j]){
+                c++;i++;j++;
+            }
+            else j++;
+        }
+        return c;
+    }
+};
