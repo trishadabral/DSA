588abcfb14a465ab1a11bd1081a324feb8140312
Trisha Dabral <trishadabral@gmail.com>
2026-09-13 23:49:26 +0530

    Time: 85 ms (86.94%) | Memory: 28 MB (81.48%) - LeetSync

---
+++ 454-4sum-ii/4sum-ii.cpp
@@ -0,0 +1,18 @@
+class Solution{
+public:
+    int fourSumCount(vector<int>& nums1,vector<int>& nums2,vector<int>& nums3,vector<int>& nums4){
+        unordered_map<int,int>m;
+        for(int a:nums1){
+            for(int b:nums2){
+                m[a+b]++;
+            }
+        }
+        int count=0;
+        for(int c:nums3){
+            for(int d:nums4){
+                count+=m[-(c+d)];
+            }
+        }
+        return count;
+    }
+};
